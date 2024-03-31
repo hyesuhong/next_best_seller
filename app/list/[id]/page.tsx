863fde589131859ccdde0a361e314c721a35338f
Hyesu <honghs95@gmail.com>
2024-04-01 02:48:20 +0900

    feat: add list detail page

---
+++ app/list/[id]/page.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { GetBooks } from '@/types/api';
+import { Params } from '@/types/params';
+
+async function getBooks(id: string) {
+	const url = `https://books-api.nomadcoders.workers.dev/list?name=${id}`;
+
+	const response = await fetch(url);
+	const data = await response.json();
+	return data as GetBooks;
+}
+
+export async function generateMetadata({ params: { id } }: Params) {
+	const books = await getBooks(id);
+
+	return {
+		title: books.results.display_name,
+	};
+}
+
+export default async function Page({ params: { id } }: Params) {
+	const books = await getBooks(id);
+
+	return (
+		<main>
+			<h1>{books.results.display_name}</h1>
+			<article>
+				{books.results.books.map((book) => (
+					<dl key={book.primary_isbn13}>
+						<dt>
+							<Image
+								src={book.book_image}
+								alt={book.title}
+								width={book.book_image_width}
+								height={book.book_image_height}
+							/>
+						</dt>
+						<dd>
+							<h4>{book.title}</h4>
+							<p>{book.author}</p>
+							<Link href={book.amazon_product_url}>Buy Now</Link>
+						</dd>
+					</dl>
+				))}
+			</article>
+		</main>
+	);
+}
