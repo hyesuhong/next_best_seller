9995df8e194dee75826f2c2808772b311ba0c464
Hyesu <honghs95@gmail.com>
2024-04-01 02:12:29 +0900

    feat: add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+	return (
+		<main>
+			<h1>About Us</h1>
+			<p>
+				Welcome to the official explorer for The New York Times Best Seller list
+				explorer.
+				<br />
+				We hope you enjoy your stay!
+			</p>
+		</main>
+	);
+}
